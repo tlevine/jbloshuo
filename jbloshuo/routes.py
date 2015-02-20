5e0307037678c0fb97e1ece6cf5a847846d03ea6
Thomas Levine <_@thomaslevine.com>
2015-02-20 20:52:02 +0000

    -

--- jbloshuo/routes.py
+++ jbloshuo/routes.py
@@ -6,8 +6,6 @@ from bottle import Bottle, request, response, \
                    view, TEMPLATE_PATH, \
                    static_file
 
-from .queries import hierarchy, subhierarchy
-
 LIB_DIR = os.path.split(__file__)[0]
 TEMPLATE_PATH.append(os.path.join(LIB_DIR, 'views'))
 app = Bottle()
@@ -17,6 +15,40 @@ app = Bottle()
 def home():
     return {}
 
+@app.route('/consultant')
+@view('form')
+def consultant():
+    return {
+        'category': 'consultant',
+        'category_pretty': 'a consultant',
+        'description_noun': 'project',
+        'pay_label': 'What is your budget? (Choose the closest one.)',
+        'pay_options': [
+            '$4,000',
+            '$16,000',
+            '$64,000',
+            '$256,000 (or more)',
+        ],
+        'initiative_label': 'Why are you interested in conducting this project? For example, is this part of a larger initiative?',
+    }
+
+@app.route('/employee')
+@view('form')
+def employee():
+    return {
+        'category': 'employee',
+        'category_pretty': 'an employee',
+        'description_noun': 'position',
+        'pay_label': 'What is the salary? (Choose the closest one.)',
+        'pay_options': [
+            '$64,000',
+            '$128,000',
+            '$256,000',
+            '$512,000',
+        ],
+        'initiative_label': 'Why are you interested in conducting this project? For example, is this part of a larger initiative?',
+    }
+
 @app.route('/submit')
 @view('submit')
 def submit():
@@ -25,70 +57,3 @@ def submit():
 @app.route('/style.css')
 def css():
     return static_file('style.css', root = LIB_DIR)
-
-"""
-
-@app.get('/!/<querystr:path>/')
-@view('thread')
-def search(querystr):
-    db = Database()
-    query = Query(db, querystr)
-    if query.count_messages() == 1:
-        message = next(iter(query.search_messages()))
-        title = message.get_header('subject')
-        try:
-            parts = [(i + 1, part.get_filename('No description')) \
-                     for i, part in enumerate(message.get_message_parts())]
-            body = message.get_part(1)
-        except UnicodeDecodeError:
-            parts = []
-            body = 'There was an encoding problem with this message.'
-    else:
-        title = 'Results for "%s"' % querystr
-        parts = []
-        body = None
-
-    return {
-        'title': title,
-        'parts': parts,
-        'body': body,
-        'threads': list(hierarchy(query)),
-    }
-
-@app.get('/!/<querystr:path>/<n:int>')
-def attachment(querystr, n):
-    db = Database()
-    query = Query(db, querystr)
-    if query.count_messages() != 1:
-        redirect('/!/%s/' % querystr)
-    else:
-        message = next(iter(query.search_messages()))
-        parts = message.get_message_parts()
-        i = n - 1
-        if i >= len(parts):
-            redirect('/!/%s/' % querystr)
-        else:
-            part = parts[i]
-            content_type = part.get_content_type()
-            response.content_type = content_type
-         #  response.charset = part.get_content_charset()
-
-            fn = part.get_filename()
-            if fn != None:
-                response.headers['content-disposition'] = 'filename="%s";' % unidecode(fn).replace('"', '')
-
-            payload = message.get_part(n)
-            if 'html' in content_type.lower():
-                return clean_html(payload)
-            else:
-                return payload
-
-@app.route('/!')
-@app.route('/!/')
-def exclaim():
-    redirect('/')
-
-@app.route('/!/<querystr:path>')
-def slash(querystr):
-    redirect('/!/' + querystr.rstrip('/') + '/')
-"""
--- jbloshuo/views/form.tpl
+++ jbloshuo/views/form.tpl
@@ -1,15 +1,32 @@
-<h1>Hire a {{category_pretty}}</h1>
+<h1>Hire {{category_pretty}}</h1>
 <form>
+  <label name="name">What is your name?</label>
+  <input name="name" type="text" />
+
+  <label name="email">What is your email address?</label>
+  <input name="email" type="email" />
+
+  <label name="company">What is the company, if you are able to tell me?</label>
+  <input name="company" type="text">
+
   <label name="description">{{description_label}}</label>
-  <textarea placeholder="Write about the job, or provide a link to a description."
+  <textarea placeholder="Write about the {{description_noun}}, or provide a link to a description."
             name="description"></textarea>
 
   <label name="pay">{{pay_label}}</label>
   <select name="pay">
-    % for option, pretty_option in pay_options:
-    <option value="{{option}}">{{pretty_option}}</option>
+    % for option in pay_options:
+    <option value="{{option}}">{{option}}</option>
     % end
   </select>
+
+  % if category == 'employee':
+  <label name="time">Is this position part-time or full-time?</label>
+  <select name="time">
+    <option value="part-time">Part-time</option>
+    <option value="full-time">Full-time</option>
+  </select>
+  % end
 
   <label name="initiative">{{initiative_label}}</label>
   <input type="text" name="initiative" />
